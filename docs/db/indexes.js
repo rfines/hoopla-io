8c130bc5be1fafa93322eee666ea5532d5798d21
Robert <robert@localruckus.com>
2013-07-29 16:18:33 -0500

    Added postal code index

--- docs/db/indexes.js
+++ docs/db/indexes.js
@@ -1,3 +1,4 @@
 db.postalCode.ensureIndex( { geo : "2dsphere" } )
 db.business.ensureIndex( { geo : "2dsphere" } )
 db.event.ensureIndex( { geo : "2dsphere" } )
+db.postalCode.ensureIndex( {"code" : 1})
