4cb36b9bdbb68ca957837d013e5ccfff794b9f70
Adam England <adam.n.england@gmail.com>
2013-10-17 14:27:41 -0500

    Updating seat geek jobs

--- docs/db/migrations/1-addSeatGeekRound1.js
+++ docs/db/migrations/1-addSeatGeekRound1.js
@@ -1,1 +1,2 @@
-db.aggregationJobs.save({name: 'SEATGEEK-STL', provider: 'seatgeek', postalCode: '65205', nextRun: new Date()})
+db.aggregationJob.save({name: 'SEATGEEK-STL', provider: 'seatgeek', postalCode: '63112', nextRun: new Date()})
+db.aggregationJob.save({name: 'SEATGEEK-DENVER', provider: 'seatgeek', postalCode: '80203', nextRun: new Date()})
