697d0eb9673ae00d7a28c46bd64a734aa24bfc63
Adam England <adam.n.england@gmail.com>
2013-10-28 15:03:43 -0500

    Adding seat geek sources

---
+++ docs/db/migrations/4-addSeatGeekRound2.js
@@ -0,0 +1,3 @@
+db.aggregationJob.save({name: 'SEATGEEK-DALLAS', provider: 'seatgeek', postalCode: '75202', nextRun: new Date()})
+db.aggregationJob.save({name: 'SEATGEEK-DESMOINES', provider: 'seatgeek', postalCode: '50309', nextRun: new Date()})
+db.aggregationJob.save({name: 'SEATGEEK-OMAHA', provider: 'seatgeek', postalCode: '68132', nextRun: new Date()})
