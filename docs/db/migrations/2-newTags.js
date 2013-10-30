9db8ca3b52d2f1c4fff10f9aa8e512bc290bb155
Robert <robert@localruckus.com>
2013-10-30 16:14:55 -0500

    added tags for eventbrite and topeka job

--- docs/db/migrations/2-newTags.js
+++ docs/db/migrations/2-newTags.js
@@ -1,3 +1,18 @@
 db.eventTag.save({text:"College Sports",slug:"COLLEGE-SPORTS"});
 db.eventTag.save({text:"Sports", slug:"SPORTS"});
 db.eventTag.save({text:"Auto Racing", slug:"AUTO-RACING"});
+
+//new tags for event brite
+db.eventTag.save({text:"Conference", slug:"CONFERENCE"});
+db.eventTag.save({text:"Conventions", slug:"CONVENTIONs"});
+db.eventTag.save({text:"Fundraisers", slug:"FUNDRAISERS"});
+db.eventTag.save({text:"Meetings", slug:"MEETINGS"});
+db.eventTag.save({text:"Performance", slug:"PERFORMANCE"});
+db.eventTag.save({text:"Runions", slug:"REUNIONS"});
+db.eventTag.save({text:"Sales", slug:"SALES"});
+db.eventTag.save({text:"Seminars", slug:"SEMINARS"});
+db.eventTag.save({text:"Social", slug:"SOCIAL"});
+db.eventTag.save({text:"Tradeshows", slug:"TRADESHOWS"});
+db.eventTag.save({text:"Travel", slug:"TRAVEL"});
+db.eventTag.save({text:"Religion", slug:"RELIGION"});
+db.eventTag.save({text:"Recreation", slug:"RECREATION"});
--- docs/db/migrations/4-addSeatGeekRound2.js
+++ docs/db/migrations/4-addSeatGeekRound2.js
@@ -1,3 +1,4 @@
 db.aggregationJob.save({name: 'SEATGEEK-DALLAS', provider: 'seatgeek', postalCode: '75202', nextRun: new Date()})
 db.aggregationJob.save({name: 'SEATGEEK-DESMOINES', provider: 'seatgeek', postalCode: '50309', nextRun: new Date()})
 db.aggregationJob.save({name: 'SEATGEEK-OMAHA', provider: 'seatgeek', postalCode: '68132', nextRun: new Date()})
+db.aggregationJob.save({name: 'SEATGEEK-TOP', provider: 'seatgeek', postalCode: '66604', nextRun: new Date()})
